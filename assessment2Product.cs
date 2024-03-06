e832d4ce51639c446156ccc5c5d41669cc3fccdf
SankalpInfinite <161150225+SankalpInfinite@users.noreply.github.com>
2024-03-06 10:38:35 +0530

    Add files via upload
    Asessment2

---
+++ assessment2Product.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assessment
+{
+    public class assessment2Product
+    {
+       
+        public int ProductID;
+        public string ProductName;
+        public decimal Price;
+    }
+
+    class abc
+    {
+        static void Main(string[] args)
+        {
+            
+            List<assessment2Product> products = new List<assessment2Product>();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"Enter details for Product {i}:");
+                Console.Write("Product ID: ");
+                int productId = int.Parse(Console.ReadLine());
+
+                Console.Write("Product Name: ");
+                string productName = Console.ReadLine();
+
+                Console.Write("Price: ");
+                decimal price = decimal.Parse(Console.ReadLine());
+
+                products.Add(new assessment2Product
+                {
+                    ProductID = productId,
+                    ProductName = productName,
+                    Price = price
+                });
+            }
+
+            products = products.OrderBy(p => p.Price).ToList();
+
+            Console.WriteLine("\nSorted Products (by price):");
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Product ID: {product.ProductID}, Name: {product.ProductName}, Price: {product.Price:C}");
+            }
+        }
+    }
+}
---
+++ assessment2Student.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assessment
+{
+    public abstract class assessment2Student
+    {
+       String Name;
+       int StudentId;
+       float Grade;
+        
+        protected assessment2Student(String Name,int StudentId)
+        {
+           Name = Name;
+            StudentId = StudentId;
+        }
+        abstract public  Boolean Ispassed( float Grade);
+    }
+    class Undergraduate : assessment2Student
+    {
+        public Undergraduate(String Name, int StudentId) : base( Name, StudentId) { }
+        public override bool Ispassed(float grade)
+        {
+            if (grade > 70)
+                return true;
+            return false;
+        }
+    }
+    class Graduate : assessment2Student {
+        public Graduate(String Name, int StudentId) : base(Name, StudentId) { }
+        public override bool Ispassed(float grade)
+        {
+            if (grade > 80)
+                return true;
+            return false;
+        }
+    }
+    class calling
+    {
+    public static void Main()
+    {
+            Console.WriteLine("Enter your Name");
+            String Name = Console.ReadLine();
+            Console.WriteLine("Enter your Student ID");
+            int StudentId = Convert.ToInt32(Console.Read());
+            Console.WriteLine("Enter your Grade");
+            float Grade = float.Parse(Console.ReadLine());
+            Graduate g = new Graduate(Name,StudentId);
+            Boolean ch= g.Ispassed(Grade);
+            if(ch==true)
+                Console.WriteLine($" Student is Pass");
+            else
+                Console.WriteLine($" Student is Fail");
+
+
+            Undergraduate ug = new Undergraduate(Name, StudentId);
+            Boolean ch1 = ug.Ispassed(Grade);
+            if (ch == true)
+                Console.WriteLine($" Student is Pass");
+            else
+                Console.WriteLine($" Student is Fail");
+
+            Console.Read();
+    }
+}
+}
